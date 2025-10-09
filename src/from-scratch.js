d5b129d293c6273f18245c76e27f123d41862c86
Marcy Student <Marcy_Student@mac.mynetworksettings.com>
2025-10-08 23:25:27 -0400

    problem solved for stings

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,25 +1,85 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+if (inches === 0) {
+  return 'drought'
+} else if (inches < 2){
+  return 'dry'
+} else if (inches < 4){
+  return 'average' 
+} else if (inches < 6){
+  return 'rainy'
+} else if (inches >= 6){
+ return 'flood'
+}
 };
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+if (breed === 'snake'){
+  return 'Hiss hiss!'
+} else if (breed === 'cat' && age < 5){
+  return 'Mew mew!'
+} else if (breed === 'cat' && age >= 5){
+  return 'Meow meow!'
+} else if (breed === 'dog' && age < 5){
+  return 'Arf arf!'
+} else if (breed === 'dog' && age >= 5 && age < 10){
+  return 'Woof woof!'
+} else if (breed === 'dog' && age >= 10){
+  return 'Boof!'
+} else {
+  return 'Happy birthday!'
+}
 };
 
-const funTypes = () => {
+const funTypes = (jsType) => {
+if (jsType === 'string') {
+  return "That's just some text."
+} else if (typeof jsType === 'number'){
+  return "That's a good number."
+} else if (typeof jsType === 'boolean'){
+  return "To bool, or not to bool?"
+} else if (typeof jsType === 'undefined' ){
+  return "Nothing, but I didn't set that."
+} else if (jsType === null){
+  return "Nothing, and I did set that."
+} else if (typeof jsType === 'object'){
+  return "Anybody got the key?"
+} else if (Array.isArray(jsType)){
+  "I order you to be indexed."
+} else if (Number.isNaN(jsType)){
+  return "Well, now you're just showing off." 
+}
 };
 
-const rounder = () => {
+const rounder = (float, roundingSetting ) => {
+if (roundingSetting === "up" ){
+  return Math.ceil(float)
+} else if (roundingSetting === "down") {
+  return Math.floor(float) 
+} else if (roundingSetting === "honest"){
+  return  Math.round(float) 
+} else {
+  return 'invalid for this'
+}
 };
 
-const formatName = () => {
+const formatName = (first, last) => {
+let capitalized1 = first[0].toUpperCase() + first.slice(1).toLowerCase();
+let capitalized2 = last[0].toUpperCase() + last.slice(1).toLowerCase();
+return `${capitalized1} ${capitalized2}`
+}
+
+const extractDomain = (email) => {
+  let symbol = '@'
+  let email.indexOf('@')
+  let email.slice()
 };
 
-const extractDomain = () => {
-};
+const startsWithVowel = (str) => {
 
-const startsWithVowel = () => {
-};
+}
 
 const rotate = () => {
+
 };
 
 module.exports = {
